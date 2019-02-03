fa98788330594295043770009154c98e48ed2def
jeizsm <jeizsm@gmail.com>
2019-02-03 12:06:56 +0300

    bind keys only in interactive mode

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,11 +1,13 @@
-# Bind word expansion (and command completion) to the Tab key.
-bind --sets-mode expand \t expand:execute
+if status --is-interactive
+  # Bind word expansion (and command completion) to the Tab key.
+  bind --sets-mode expand \t expand:execute
 
-# During expansion, bind Backspace to revert the operation.
-bind --mode expand --sets-mode default --key backspace expand:revert
+  # During expansion, bind Backspace to revert the operation.
+  bind --mode expand --sets-mode default --key backspace expand:revert
 
-# Bind Tab to cycle through the available expansions.
-bind --mode expand \t expand:choose-next
+  # Bind Tab to cycle through the available expansions.
+  bind --mode expand \t expand:choose-next
 
-# If the user enters any key other than Backspace, exit expand mode and passthrough keys to the default binding.
-bind --mode expand --sets-mode default '' ''
+  # If the user enters any key other than Backspace, exit expand mode and passthrough keys to the default binding.
+  bind --mode expand --sets-mode default '' ''
+end
